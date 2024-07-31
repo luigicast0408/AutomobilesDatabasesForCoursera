ee44fea7a27826292b0ebe8e11742dfa9d450ba0
Luigi Castano <115658266+luigicast0408@users.noreply.github.com>
2024-07-31 08:05:45 +0200

    fix(index.php): fix path Login page

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <!DOCTYPE html>
-<html>
 <head>
     <title>Your Name</title>
 </head>
 <body>
 <h1>Welcome to Autos Database</h1>
-<p><a href="login.php">Please Log In</a></p>
+<p><a href="/Login/login.php">Please Log In</a></p>
 </body>
-</html>
-<?php
